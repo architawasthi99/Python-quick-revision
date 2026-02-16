6bdcf7d65677302aa69062d6f867a0a1035f0c0d
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-02-16 14:06:34 +0530

    Add files via upload

---
+++ exceptionHandling.py
@@ -0,0 +1,9 @@
+a="archit"
+try:
+    print(f"Multiplicatiion table of {a} is: ")
+    for i in range(1,11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except Exception as e:
+    print("INVALID INPUT")
+print("hello")
+print("world")    
